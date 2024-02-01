70fdb223ccd53f3497160c514cc8afdad5c16f4f
Yihao Wang <yihao.w@nyu.edu>
2024-02-01 14:21:44 -0500

    render in `<pre>` element

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import * as wasm from "hello-wasm-pack";
+import { 
+    greet, 
+    Universe 
+} from "wasm-game-of-life";
 
-wasm.greet();
+greet();
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+  
+    requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+  
